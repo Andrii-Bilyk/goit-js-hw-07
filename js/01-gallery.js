49c40a12f0cf739e78f0dccb4df324e6af9aed25
Andrii Bilyk <bilykaim@gmail.com>
2023-09-22 21:40:38 +0300

    update gallery and lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryUl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+  })
+  .join('');
+
+  galleryUl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+let modal = null;
+const openModal = (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') return;
+
+    modal = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+
+  modal.show();
+  document.addEventListener('keydown', closeModalByEscape);
+};
+
+const closeModalByEscape = (event) => {
+  if (event.code !== 'Escape') return;
+  modal.close();
+  document.removeEventListener('keydown', closeModalByEscape);
+};
+
+galleryUl.addEventListener('click', openModal);
